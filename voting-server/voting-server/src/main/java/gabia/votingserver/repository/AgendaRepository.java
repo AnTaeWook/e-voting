40199ec8b963871c9c98a0a173c4439bc989b5e3
Endo <twahn@gabia.com>
2023-01-09 17:37:14 +0900

    #3 - 리포지토리 구현
    JPA repository 를 이용하여 안건 리포지토리를 
    구현하였음
    
    전체 안건 조회를 위한 메서드를 메서드 명명 규칙을 활용하여 작성하였음.

---
+++ voting-server/voting-server/src/main/java/gabia/votingserver/repository/AgendaRepository.java
@@ -0,0 +1,11 @@
+package gabia.votingserver.repository;
+
+import gabia.votingserver.domain.Agenda;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.awt.print.Pageable;
+import java.util.List;
+
+public interface AgendaRepository extends JpaRepository<Agenda, Long> {
+    public List<Agenda> findAllOrderByStartsAt(Pageable pageable);
+}
