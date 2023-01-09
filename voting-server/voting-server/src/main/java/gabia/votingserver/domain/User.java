2581f42bd3a27bad2f576f12b2d79e53198603a1
Endo <twahn@gabia.com>
2023-01-09 16:00:34 +0900

    #2 - 도메인 작성
    User의 역할을 enum 으로 정의하고,
    인증 대상인 User 를 UserDetails 상속 받아 구현하였음.
    
    User가 지니는 권한은 리스트가 아닌 단일 권한으로 소유하도록 하였음.
    
    추가로, 실제 데이터베이스에는 추후 역할 추가를 고려해
    String 타입으로 insert 되도록 하였음.

---
+++ voting-server/voting-server/src/main/java/gabia/votingserver/domain/User.java
@@ -0,0 +1,72 @@
+package gabia.votingserver.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class User implements UserDetails {
+
+    @Id
+    @Column(updatable = false, unique = true, nullable = false, length = 50)
+    private String userId;
+
+    @Column(nullable = false, length = 50)
+    private String password;
+
+    @Column(nullable = false, length = 50)
+    private String name;
+
+    @Column(nullable = false, length = 10)
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    private Integer voteRights;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return userId;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
