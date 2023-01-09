8ac21a1e4f6c6309519dbd53b57e7c495141b499
Endo <twahn@gabia.com>
2023-01-09 16:13:02 +0900

    #2 - DA, 서비스 구현
    사용자의 정보를 등록하고, 가져오기 위한 리포지토리를
    JPA Repository 통하여 구현하고,
    
    이를 활용한 사용자 서비스와 시큐리티가 활용할 
    유저 디테일 서비스를 완성하였음.

---
+++ voting-server/voting-server/src/main/java/gabia/votingserver/service/CustomUserDetailsService.java
@@ -0,0 +1,35 @@
+package gabia.votingserver.service;
+
+import gabia.votingserver.domain.User;
+import gabia.votingserver.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUserId(username)
+                .map(this::createUserDetails)
+                .orElseThrow(() -> new UsernameNotFoundException("해당하는 유저를 찾을 수 없습니다."));
+    }
+
+    private UserDetails createUserDetails(User user) {
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getUserId())
+                .password(user.getPassword())
+                .roles(List.of(user.getRole().toString()).toArray(new String[0]))
+                .build();
+    }
+}
